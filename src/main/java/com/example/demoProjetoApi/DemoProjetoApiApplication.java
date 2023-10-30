a49444e4cd93fd563070e4e5e39f1af7db5d1ef0
NelsonModenezNeto <nelsonmodenezneto@hotmail.com>
2023-10-30 19:21:51 -0300

    Initial commit

---
+++ src/main/java/com/example/demoProjetoApi/DemoProjetoApiApplication.java
@@ -0,0 +1,13 @@
+package com.example.demoProjetoApi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoProjetoApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoProjetoApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/demoProjetoApi/DemoProjetoApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.demoProjetoApi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoProjetoApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
